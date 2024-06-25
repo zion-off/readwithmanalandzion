40c6ee7871f48a2c93d3843ca9cc6cb95e6439b6
zion-off <ajz317@nyu.edu>
2024-06-25 12:42:48 +0600

    testing chromium

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+  },
 };
 
 export default nextConfig;
