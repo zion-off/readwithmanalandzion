180d4acf1339087cfbb822c403c5465c50bec3d4
zion <ajz317@nyu.edu>
2024-06-29 20:21:02 +0600

    updated viewport metadata

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,15 @@ import "./globals.css";
 
 export const metadata = {
   title: "shelf",
-  description: "",
+  description: ""
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: "no"
 };
 
 export default function RootLayout({ children }) {
